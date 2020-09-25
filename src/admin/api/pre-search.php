4c0cca7caee934e96ee9623877b6e70388cac42c
Nguyen Thanh <thanhnguyennguyen1995@gmail.com>
2020-09-25 16:09:29 +0700

    .

--- src/admin/api/pre-search.php
+++ src/admin/api/pre-search.php
@@ -8,11 +8,25 @@
    require_once __DIR__."/../modules/ErrorMS.php";
    require_once __DIR__."/../modules/Method.php";
    
+   $SQL -> query("create table if not exists Keywords (
+         id int null null,
+         keyword tinytext not null,
+         idApp int not null,
+         point int not null,
+         primary key(id)
+      )");
+   
    new class {
       public function __construct() {
          global $SQL;
          if ( $_SERVER["REQUEST_METHOD"] == "GET" ) {
             if ( ($_GET["query"] ?? null) != null ) {
+               
+               $apps = Method::fetch_query("select name, id from Apps where name like '%%1%'", [], $_GET["query"]);
+               
+               $max = Method::getAppMaxPoint($apps, $_GET["query"]);
+               
+               $SQL -> query("insert into Keywords ( keyword, idApp, point ) values ( '".addslashes($_GET["query"])."', $max[ID], $max[point] )");
                
                echo json_encode([
                   "state" => [
@@ -20,7 +34,7 @@
                      "code" => 200,
                      "message" => ""
                   ],
-                  "data" => Method::fetch_query("select name, id from Apps where name like '%%1%'", [], $_GET["query"])
+                  "data" => $apps
                ]);
             } else {
                echo json_encode(ErrorMS::PARAMS);
--- src/admin/modules/Method.php
+++ src/admin/modules/Method.php
@@ -1,6 +1,61 @@
 <?php
    require_once __DIR__."/SQL.php";
    require_once __DIR__."/File.php";
+   
+   function sameString( string $string, string $keyword ): number {
+      for ( $length = count($keyword) - 1; $length > 0; $length-- ) {
+         if ( strpos(
+            str_replace("/\s/g", "", $string),
+            substr(
+               str_replace("/\s/g", "", $keyword),
+               $length)
+            ) !== false ) {
+            return $length;
+         }
+      }
+      return 0;
+   }
+   
+   function getPointKeyword( array $app, string $keyword, array $dataField = [] ): number {
+      
+      $spoint = 0;
+      
+      foreach ( $dataField as $__proto__ => $value ) {
+         $spoint = max( $spoint, sameString($value, $__proto__) * $value );
+      }
+      
+      return $spoint;
+   }
+    
+   function getAppMaxPoint( array $apps, string $keyword ): array {
+      $points = array_map(function ( $item ) use ( $keyword ) {
+         return [
+            "id" => $item["id"],
+            "value" => getPointKeyword($item, $keyword, [
+               "name" => 70,
+               "developer" => 50,
+               "description" => 40,
+               "category" => 30,
+               "compatibility" => 20,
+               "languages" => 10
+            ])
+         ];
+      }, $apps);
+      
+      $max = [
+         "id" => null,
+         "value" => -Infinite
+      ];
+      
+      foreach ( $points as $index => $value ) {
+         if ( $value["point"] > $max["point"] ) {
+            $max = $value;
+         }
+      }
+      
+      return $max;
+   }
+   
    class Method {
       static public function unset_cache(&$array) {
          for ( $i = 0, $count = count($array); $i < $count; $i++ ) {
@@ -34,6 +89,9 @@
          global $SQL;
          
          return static::fetch_array($SQL -> query($query), $unPath);
+      }
+      static public function getAppMaxPoint( array $apps, string $keyword ): array {
+         return getAppMaxPoint($apps, $keyword);
       }
    }
 ?>
